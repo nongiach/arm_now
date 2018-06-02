94d85d8a46fdde2345b9d5cb08e45c00346bd806
chaign_c <betsizaraina.chaignon@epitech.eu>
2018-06-02 19:51:14 +0200

    sync

--- arm_now/options.py
+++ arm_now/options.py
@@ -19,3 +19,27 @@ def autostart(rootfs, script):
         fs.put(script, "/etc/init.d/S90_user_autostart", right=555)
     else:
         fs.rm("/etc/init.d/S90_user_autostart")
+
+def sync(rootfs, src, dest):
+    fs = Filesystem(rootfs)
+    if not fs.implemented():
+        return
+    print("Adding current directory to the filesystem..")
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        files = [ i for i in os.listdir(".") if i != "arm_now" and not i.startswith("-") ]
+        if files:
+            tar = tmpdirname + "/current_directory.tar"
+            subprocess.check_call(["tar", "cf", tar] + files)
+            subprocess.check_call("e2cp -G 0 -O 0".split(' ') + [tar, rootfs + ":/"])
+            fs.create("/etc/init.d/S95_sync_current_diretory","""
+                        cd {dest}
+                        tar xf /current_directory.tar
+                        rm /current_directory.tar
+                        """.format(dest=dest), right=555)
+
+    # TODO: check rootfs fs against parameter injection
+    fs.create("/sbin/save", """
+                cd {dest}
+                tar cf /root.tar *
+                sync
+                """.format(dest=dest), right=555)
--- preconfigure.sh
+++ preconfigure.sh
@@ -1,7 +1,18 @@
+cd ../
+rm -rf preconfig
+mkdir preconfig
+cd preconfig
 arm_now clean
+mkdir cacert
+cd cacert
+curl https://curl.haxx.se/ca/cacert.pem | awk 'split_after==1{n++;split_after=0} /-----END CERTIFICATE-----/ {split_after=1} {print > "cert" n ".pem"}'
 cat <<EOF>>autostart.sh
 cd /root
 ./install_pkg_manager.sh
+opkg install wget
+rm /usr/bin/wget
+sed 's/http:/https:/g' /opt/etc/opkg.conf > /opt/etc/opkg.conf.bak
+mv /opt/etc/opkg.conf.bak /opt/etc/opkg.conf
 poweroff
 EOF
-arm_now start --autostart autostart.sh
+arm_now start --autostart autostart.sh --sync
